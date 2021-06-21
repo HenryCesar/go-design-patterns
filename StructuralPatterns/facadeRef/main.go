d44ceaa59cae315680a5a20f0dc2d6433871e76a
Henrique C. P. S. Almeida <henrique.almeida@gruposplice.com.br>
2021-06-21 09:37:15 -0300

    feat(main.go): with this code is possible understand the principle of facade

---
+++ StructuralPatterns/facadeRef/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// walletFacade is a struct that contains the components of an account
+type walletFacade struct {
+	account      *account
+	wallet       *wallet
+	securityCode *securityCode
+	notification *notification
+	ledger       *ledger
+}
+
+func newWalletFacade(accountID string, code int) *walletFacade {
+	fmt.Println("Starting create account")
+	walletFacade := &walletFacade{
+		account:      newAccount(accountID),
+		securityCode: newSecurityCode(code),
+		wallet:       newWallet(),
+		notification: &notification{},
+		ledger:       &ledger{},
+	}
+	fmt.Println("Account created")
+	return walletFacade
+}
+
+func (w *walletFacade) addMoneyToWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting add money to wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	w.wallet.creditBalance(amount)
+	w.notification.sendWalletCreditNotification()
+	w.ledger.makeEntry(accountID, "credit", amount)
+	return nil
+}
+
+func (w *walletFacade) deductMoneyFromWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting debit money from wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	err = w.wallet.debitBalance(amount)
+	if err != nil {
+		return err
+	}
+	w.notification.sendWalletDebitNotification()
+	w.ledger.makeEntry(accountID, "debit", amount)
+	return nil
+}
+
+// Setting account functions [Subsystem parts]
+type account struct {
+	name string
+}
+
+func newAccount(accountName string) *account {
+	return &account{
+		name: accountName,
+	}
+}
+
+func (a *account) checkAccount(accountName string) error {
+	if a.name != accountName {
+		return fmt.Errorf("account name is incorrect")
+	}
+	fmt.Println("Account Verified")
+	return nil
+}
+
+// Setting security code functions [Subsystem parts]
+type securityCode struct {
+	code int
+}
+
+func newSecurityCode(code int) *securityCode {
+	return &securityCode{
+		code: code,
+	}
+}
+
+func (s *securityCode) checkCode(incomingCode int) error {
+	if s.code != incomingCode {
+		return fmt.Errorf("security code is incorrect")
+	}
+	fmt.Println("SecurityCode Verified")
+	return nil
+}
+
+// Setting Wallet functions [Subsystem Parts]
+type wallet struct {
+	balance int
+}
+
+func newWallet() *wallet {
+	return &wallet{
+		balance: 0,
+	}
+}
+
+func (w *wallet) creditBalance(amount int) {
+	w.balance += amount
+	fmt.Println("Wallet balance added successfully")
+}
+
+func (w *wallet) debitBalance(amount int) error {
+	if w.balance < amount {
+		return fmt.Errorf("balance is not sufficient")
+	}
+	fmt.Println("Wallet balance is Sufficient")
+	w.balance -= amount
+	return nil
+}
+
+// Setting Ledger functions [Subsystem Parts]
+type ledger struct{}
+
+func (s *ledger) makeEntry(accountID, txnType string, amount int) {
+	fmt.Printf("Make ledger entry for accountID '%s' with txnType '%s' for amount %d\n", accountID, txnType, amount)
+}
+
+// Setting Notifications functions [Subsystem Parts]
+type notification struct{}
+
+func (n *notification) sendWalletCreditNotification() {
+	fmt.Println("Sending wallet credit notification")
+}
+
+func (n *notification) sendWalletDebitNotification() {
+	fmt.Println("Sending wallet debit notification")
+}
+
+// Main Function [Client Code]
+func main() {
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
