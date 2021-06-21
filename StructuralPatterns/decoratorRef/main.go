3f9ac9996d99df18a8dc4378f2103ecf25863de2
Henrique C. P. S. Almeida <henrique.almeida@gruposplice.com.br>
2021-06-21 09:10:46 -0300

    feat(facadeRef & README.md): folder to contain the facade content and a readme for more explanation

--- StructuralPatterns/decoratorRef/main.go
+++ StructuralPatterns/decoratorRef/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Get the pizza's price [Component interface]
 type pizza interface {
 	getPrice() int
@@ -10,4 +12,42 @@ type veggeMania struct{}
 
 func (p *veggeMania) getPrice() int {
 	return 15
+}
+
+// The type of Structural Pattern [Concrete Decorator]
+type tomatoTopping struct {
+	pizza pizza
+}
+
+func (c *tomatoTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 7
+}
+
+// The type of Structural Pattern [Concrete Decorator]
+type cheeseTopping struct {
+	pizza pizza
+}
+
+func (c *cheeseTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 10
+}
+
+// Main function [Client Code]
+func main() {
+
+	pizza := &veggeMania{}
+
+	//Add cheese topping
+	pizzaWithCheese := &cheeseTopping{
+		pizza: pizza,
+	}
+
+	//Add tomato topping
+	pizzaWithCheeseAndTomato := &tomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggeMani with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
 }
